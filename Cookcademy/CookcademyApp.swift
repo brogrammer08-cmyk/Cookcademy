25066b976e50c5bd6c5a5006b6a922aa0cb8fe60
Euglen <brogrammer08@gmail.com>
2026-04-02 21:12:20 +0200

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Euglen on 2.4.26.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
